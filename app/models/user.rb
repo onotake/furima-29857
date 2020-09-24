25a590a29090b173a6a678b84392454f531393b4
onotake <t_si2@yahoo.co.jp>
2020-09-24 16:57:05 +0900

    新規登録、ログイン、ログアウトの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   
   devise  :database_authenticatable, :registerable,
           :recoverable, :rememberable, :validatable
+  validates :nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birth_date, presence: true
   
   has_many :items 
   has_many :purchasers
