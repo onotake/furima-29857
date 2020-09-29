49ba8830372a8916f17e98b4c51b4c3ab0dd6086
onotake <t_si2@yahoo.co.jp>
2020-09-29 19:11:21 +0900

    商品出品機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,11 +8,16 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:user, :product_name, :description, :category_id, :condition_id, :delivery_fee_id, :delivery_area_id, :days_id, :price, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:user, :product_name, :description, :category_id, :condition_id, :delivery_fee_id, :delivery_area_id, :day_id, :price, :image).merge(user_id: current_user.id)
   end
 end
