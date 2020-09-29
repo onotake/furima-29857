ed851eb3013f57214eea3d5795afd4a0fe8b6418
onotake <t_si2@yahoo.co.jp>
2020-09-29 10:53:21 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,18 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:user, :product_name, :description, :category_id, :condition_id, :delivery_fee_id, :delivery_area_id, :days_id, :price, :image).merge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,11 @@
 class Item < ApplicationRecord
+  with_options presence: true do
+    validates :product_name, length: {maximum: 40},                                format: {with: /\A[ぁ-んァ-ン一-龥]/ } 
+    validates :description,  length: {maximum: 1000},                              format: {with: /\A[ぁ-んァ-ン一-龥]/ } 
+    validates :price,        numericality: {greater_than: 300, less_than:9999999}, format: {with: /\A[0-9]+\z/}
+  end
+
+  belongs_to       :user
+  has_one          :purchaser
+  has_one_attached :image
 end
